7385f081bcafac59e87aa0aba40c2a5ae26de8f4
tkmpypy <tkm2126@gmail.com>
2018-10-08 13:40:43 +0900

    [add] AWS Translate 叩く部分実装

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import click
 import boto3
 
+translate = boto3.client(service_name='translate', region_name='us-west-2', use_ssl=True)
 
 @click.command()
 @click.version_option(1.0)
@@ -8,10 +9,22 @@ import boto3
 @click.option('-t', '--target-language-code', help='One of the supported language codes for the source text. If the SourceLanguageCode is not "en", the SourceLanguageCode must be "en".', default='en')
 @click.argument('text', 'The text to translate. The text string can be a maximum of 5,000 bytes long. Depending on your character set, this may be fewer than 5,000 characters.')
 def cmd(source_language_code, target_language_code, text):
-    """cmd"""
-    click.echo(source_language_code)
-    click.echo(target_language_code)
-    click.echo(text)
+    """cmd
+
+    :param source_language_code:
+    :param target_language_code:
+    :param text:
+    """
+    # click.echo(source_language_code)
+    # click.echo(target_language_code)
+    # click.echo(text)
+    result = translate.translate_text(Text=text, 
+            SourceLanguageCode=source_language_code, TargetLanguageCode=target_language_code)
+    
+    # click.echo(result)
+
+    translated = result['TranslatedText']
+    click.echo(translated)
 
 
 def main():
