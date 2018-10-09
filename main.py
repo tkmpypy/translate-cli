98b76584291e6990a1bc010aa5422ceebcf094c8
takuma-k <takuma@ngen.co.jp>
2018-10-09 17:47:25 +0900

    [add] Fix to set up AWS from a configuration file

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import click
 import boto3
+import yaml
 
-translate = boto3.client(service_name='translate', region_name='us-west-2', use_ssl=True)
 
 @click.command()
 @click.version_option(1.0)
 @click.option('-s', '--source-language-code', help='One of the supported language codes for the source text. If the TargetLanguageCode is not "en", the SourceLanguageCode must be "en".', default='en')
 @click.option('-t', '--target-language-code', help='One of the supported language codes for the source text. If the SourceLanguageCode is not "en", the SourceLanguageCode must be "en".', default='en')
-@click.argument('text', 'The text to translate. The text string can be a maximum of 5,000 bytes long. Depending on your character set, this may be fewer than 5,000 characters.')
+@click.argument('text', type=click.STRING)
 def cmd(source_language_code, target_language_code, text):
     """cmd
 
@@ -15,12 +15,19 @@ def cmd(source_language_code, target_language_code, text):
     :param target_language_code:
     :param text:
     """
+
+    yml = __read_conf()
+
+    translate = boto3.client(service_name='translate', aws_access_key_id=yml['aws']['access_key'], aws_secret_access_key=yml['aws']['secret_key'], region_name='us-west-2', use_ssl=True)
     result = translate.translate_text(Text=text, 
             SourceLanguageCode=source_language_code, TargetLanguageCode=target_language_code)
     
     translated = result['TranslatedText']
     click.echo(translated)
 
+def __read_conf():
+    with open('conf.yml') as f:
+        return yaml.load(f)
 
 def main():
     """main"""
